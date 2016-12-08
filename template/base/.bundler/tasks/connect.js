c7f97252954253a39d8c4ed97db29f528127807e
Miguel Castillo <mcastillo@symphono.com>
2016-12-08 13:15:27 -0500

    adding ability to configure the connect server via connectrc

--- template/base/.bundler/tasks/connect.js
+++ template/base/.bundler/tasks/connect.js
@@ -1,33 +1,54 @@
+var path = require("path");
 var fallback = require("connect-history-api-fallback");
 var livereload = require("connect-livereload");
 var proxy = require("node-proxy-middleware");
 var url = require("url");
+var config = require(path.join(process.cwd(), ".connectrc.json"));
 
-module.exports = {
-  "dev": {
-    "options": {
-      "base": "dist",
-      "protocol": "http",
-      "port": 8045,
-      "hostname": "localhost",
-      "keepalive": true,
-      "open": "http://localhost:8045/index.html",
-      "middleware": function(connect, options, middlewares) {
-        middlewares.unshift(fallback({ index: "/index.html" }));
-        middlewares.unshift(livereload({ port: 32247 }));
-
-        // Proxy setup
-        //middlewares.unshift(configureProxy("/api-1", "https://my-server/api-1"));
-        //middlewares.unshift(configureProxy("/api-2", "https://my-server/api-2"));
-
-        return middlewares;
-      }
+var settings = Object.assign({}, config, {
+  "middleware": function(connect, options, middlewares) {
+    // Configure fallback to handle SPA configurations
+    if (config.fallback) {
+      middlewares.unshift(fallback(config.fallback));
     }
+
+    // Liverealod!!
+    if (config.livereload) {
+      middlewares.unshift(livereload(config.livereload));
+    }
+
+    // Reverse proxies for forwarding request to other end points.
+    // This is generally a good way to handle CORS restrictions
+    if (config.proxies) {
+      config.proxies.forEach(function(proxy) {
+        middlewares.unshift(configureProxy(proxy.source, proxy.target));
+      });
+    }
+
+    if (config.middlewares) {
+      Object.keys(config.middlewares).forEach(function(mwarename) {
+        middlewares.unshift(require(mwarename)(config.middlewares[mwarename]));
+      });
+    }
+
+    return middlewares;
   }
-};
+});
+
+// Clean up the config before feeding it to connect.
+delete settings.fallback;
+delete settings.livereload;
+delete settings.proxies;
+delete settings.middlewares;
 
 function configureProxy(route, destination) {
   var options = url.parse(destination);
   options.route = route;
   return proxy(options);
 }
+
+module.exports = {
+  "dev": {
+    "options": settings
+  }
+};
