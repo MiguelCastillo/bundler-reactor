78572280509a30188a6b1fc5019160741b570a5e
Miguel Castillo <manchagnu@gmail.com>
2018-01-10 13:05:28 -0500

    fixed issue with img and css assets not getting copying over correctly

--- template/base/src/index.js
+++ template/base/src/index.js
@@ -1,6 +1,6 @@
-import DOMReady from './utils/DOMReady';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DOMReady from './utils/DOMReady';
 import App from './views/App';
 import './style/reset.css';
 import 'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css';
